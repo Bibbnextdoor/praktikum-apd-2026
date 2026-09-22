8e6aa464756a7222713eff3782b182cde3d4098f
akuk7370@gmail.com <akuk7370@gmail.com>
2026-09-22 13:33:08 +0800

    selesai

---
+++ B1/posttest/posttest-apd-2/2609106045-HABIBUR_RAHMAN-PT-2.py
@@ -0,0 +1,18 @@
+makanan_1 = int (15000)
+makanan_2 = int (16000)
+makanan_3 = int (19000)
+makanan_4 = int (20000)
+makanan_5 = int (21000)
+makanan_6 = int (22000)
+harga_makanan = [makanan_1,makanan_2,makanan_3,makanan_4,makanan_5,makanan_6]
+biaya_aplikasi = int (5000)
+hasil_penjumlahan = int (harga_makanan[0] + harga_makanan[1] + harga_makanan[2] + harga_makanan[3] + harga_makanan[4] + harga_makanan[5])
+total_keseluruhan = int (hasil_penjumlahan + biaya_aplikasi)
+banyak_data = len(harga_makanan)
+rata_rata = int (total_keseluruhan / banyak_data)
+nim = int (45)
+bolean = nim != rata_rata
+print ("Total harga sebelum pajak:", hasil_penjumlahan)
+print ("Total harga yang harus dibayar: ",total_keseluruhan)
+print ("Rata-rata: ", rata_rata)
+print ("Output bolean: ", bolean)
---
+++ pertemuan1.py
@@ -0,0 +1,75 @@
+#numerik
+
+angka1 = 5
+angka2 = 10
+hasil = angka1 + angka2
+print(hasil)
+
+#float
+
+a = 3.45
+b = 1.23
+hitung = a + b
+print (hitung)
+
+#string
+nama = "Habib"
+hobi = "makan"
+print (nama)
+print (hobi)
+
+print('''Halo saya Rendy
+Saya suka belajar coding
+Hari ini saya belajar bahasa pemrograman Python''')
+
+print("aku""rajin""praktikum")
+print("aku"+"rajin")
+
+teks = "Rahmat Flowchart"
+print(teks[3])
+print(teks[3:6])
+print(teks[7:])
+print(teks[:6])
+print(teks[1:4:2])
+print(teks[:-1])
+biodata = ["Habib", 18, "2026", ]
+
+
+
+mata_kuliah = ["APD","Logika Matematika","Kalkulus"]
+print(mata_kuliah[0])
+print(mata_kuliah[1])
+print(mata_kuliah[2]) 
+print(mata_kuliah[0:1]) 
+print(mata_kuliah[:2])
+print(mata_kuliah[1:])
+print(mata_kuliah[::2])
+print(mata_kuliah[:-1])
+
+buku = {
+'judul' : 'Atomic Habits',
+'penulis' : 'James Clear',
+'halaman' : 320
+}
+...
+# Mengakses Value dalam Dictionary
+print(buku['judul'])
+print(buku['penulis'])
+print(buku['halaman'])
+
+angka = int("42")
+print(angka)
+
+ubah = float(99.99)
+print(ubah)
+
+nama = input("Masukkan nama kamu")
+print(nama)
+
+gaji = int(input("Masukkan gaji kamu"))
+print(gaji)
+
+print(2 < 5)
+print(2 < 4 )
+print(2 > 10)
+print(2 < 11)
